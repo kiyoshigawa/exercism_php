597b4c468d9488dc28ad44e27547d4839d061a80
tim <tim@twa.ninja>
2023-12-13 09:01:22 -0700

    WindowingSystem Exercise

---
+++ windowing-system/Position.php
@@ -0,0 +1,13 @@
+<?php
+
+class Position
+{
+    public $x;
+    public $y;
+
+    function __construct($y, $x)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
---
+++ windowing-system/Size.php
@@ -0,0 +1,13 @@
+<?php
+
+Class Size
+{
+    public $width;
+    public $height;
+
+    function __construct($height, $width)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+}
